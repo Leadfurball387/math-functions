d978e68d8da490ca9035741afd71e454dc0768b8
Leadfurball387 <leadfurball387@gmail.com>
2023-11-24 18:16:01 -0800

    Made small changes to prime_factorization function using vectors. Still does not work as intended.

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,16 +1,31 @@
+// DOES NOT WORK
+
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 
 void prime_fact(long long n) {
+
+	std::vector<int> factors{};
 
 	for (int i = 2; i <= n/2; i++) {
 		if (n % i == 0) {
 			
 			// If n=18, this prints 9, which is not prime factorization.
 			n = n / i;
-			std::cout << n << '\n';
+			//std::cout << n << '\n';
+			if (n % i == 0) {
+				factors.push_back(n);
+			}
+			else {
+				continue;
+			}
 		}
+	}
+
+	for (int i : factors) {
+		std::cout << i << '\n';
 	}
 
 }
