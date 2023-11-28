81efe53ccba1e3db283d100919aab2910899cfba
leadfurball387 <leadfurball387@gmail.com>
2023-11-27 20:37:28 -0800

    Added quadratic solver to 'math_funcs.h'.

--- math_funcs.h
+++ math_funcs.h
@@ -370,3 +370,42 @@ int longest_collatz_sequence() {
 
 	return greatest_start;
 }
+
+
+// Takes input ints a, b, c
+// Solves ax^2 + bx + c
+std::string quadratic_solver(int a, int b, int c) {
+
+    float xpos{}, xneg{};
+    float imagx{}, realx{};
+	float discriminant = pow(b, 2) - 4 * a * c;
+    std::string posans{}, negans{};
+    std::string ans{};
+
+	if (discriminant >= 0) {
+		xpos = (-b + sqrt(discriminant)) / (2 * a);
+		xneg = (-b - sqrt(discriminant)) / (2 * a);
+
+        ans = std::to_string(xpos) + ", " + std::to_string(xneg) + '\n';
+
+		return ans;
+	}
+	else if (discriminant < 0) {
+		// std::cout << "There are no real roots." << '\n';
+
+        discriminant *= -1;
+
+        // Cast b to floating point to ensure accurate division
+        realx = static_cast<float>(-b) / (2 * a);
+        imagx = sqrt(discriminant) / (2 * a);
+
+        posans = std::to_string(realx) + "+" + std::to_string(imagx) + "i";
+        negans = std::to_string(realx) + "-" + std::to_string(imagx) + "i";
+
+        ans = posans + ", " + negans + '\n';
+
+        return ans;
+	}
+
+    return ans;
+}
